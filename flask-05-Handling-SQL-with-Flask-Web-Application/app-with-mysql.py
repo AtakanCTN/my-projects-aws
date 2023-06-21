0f30dae9f42648c1736d5d7c55500c8db1aa8c1e
Atakan <atakandean001@gmail.com>
2023-06-21 11:54:42 +0300

    w

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'atakanrds.cl6ojtwprin1.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'rds112.cl6ojtwprin1.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
